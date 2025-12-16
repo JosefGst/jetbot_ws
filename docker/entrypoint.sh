8dcb71ebbaf47339da3602e000c99426e79266c0
josefgst <josefgstoettner@live.at>
2025-12-16 11:52:06 +0800

    add aliases and helper

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,19 +14,37 @@ then
   echo "Sourced bringup_ws"
 fi
 
-# Source the sim workspace, if built
-if [ -f /sim_ws/install/setup.bash ]
-then
-  source /sim_ws/install/setup.bash
-  echo "Sourced sim_ws"
-fi
 
-# Source the overlay workspace, if built
-if [ -f /overlay_ws/install/setup.bash ]
-then
-  source /overlay_ws/install/setup.bash
-  echo "Sourced overlay_ws"
-fi
+# Useful aliases
+alias sb='source ~/.bashrc'
+alias sw='source /${DEV_BASE}/install/setup.bash'
+alias rl='ros2 topic list'
+alias re='ros2 topic echo'
+alias tel='ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -p stamped:=true'
+
+# Useful helper functions
+# -----------------------
+# Build the entire workspace
+function rb() {
+        cd /${DEV_BASE}
+        colcon build --symlink-install
+        cd -
+}
+
+# Build selected packages
+function rbsp() {
+        cd /${DEV_BASE}
+        colcon build --symlink-install --packages-select $@
+        cd -
+}
+
+# Run rosdep install for the workspace
+function rdep() {
+        cd /${DEV_BASE}
+        sudo apt install python3-rosdep && rosdep update && rosdep install -i --from-path src --rosdistro $ROS_DISTRO -y
+        cd -
+}
+
 
 # Execute the command passed into this entrypoint
 exec "$@"
